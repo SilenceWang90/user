aef8bfb47757aa5c65a016670bf634148510d73d
wangpeng116 <wangpeng116@longfor.com>
2019-12-17 19:07:31 +0800

    登录

--- user-server/src/main/java/com/imooc/user/controller/LoginController.java
+++ user-server/src/main/java/com/imooc/user/controller/LoginController.java
@@ -11,10 +11,7 @@ import com.imooc.user.utils.ResultVOUtil;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
